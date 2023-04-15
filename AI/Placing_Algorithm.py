bd6fdc38a8662c6e95b9015be82c77f4e7f6aa13
shanf2 <shanf2@mcmaster.ca>
2023-04-14 23:17:42 -0400

    Revert "some last minute ai update."
    This reverts commit 59e9665e640a9908dda01fd3a8cda8dc1ff495c8.

--- AI/Placing_Algorithm.py
+++ AI/Placing_Algorithm.py
@@ -1,12 +1,14 @@
 import math
 from keywordVal import *
 
-#priority		feature
-#first			time
-#second			keyword
-def postScore(post,keyword,cur_time,date_posted):
+def postScore(post,keyword,star,cur_time,date_posted):
+	star_val=0
 	time_val=0
 	keyword_val=0
+
+	#if there is a star, post is given priority
+	if star == true:
+		star_val=10
 
 	#if post is within the first week(7 days) post, it is given priority
 	time_val= 0.2*math.log(-(cur_time-date_posted),10)+0.831
@@ -16,5 +18,5 @@ def postScore(post,keyword,cur_time,date_posted):
 	for key in keyword_post.keys():
 		keyword_val= keyword_val + keyword.get(key,0)
 
-	rank= time_val+keyword_val
+	rank= star_val+time_val+keyword_val
 	return rank
